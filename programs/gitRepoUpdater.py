b2e4a789be12583e243b1209275a0c8275f8448a
davidvelascogarcia <davelasc@ing.uc3m.es>
2021-02-07 16:38:24 +0100

    Transformation to class, requirements added

--- programs/gitRepoUpdater.py
+++ programs/gitRepoUpdater.py
@@ -9,141 +9,158 @@
 # Libraries
 import datetime
 import os
+from halo import Halo
 import platform
-import sys
-import time
 
-print("")
-print("")
-print("**************************************************************************")
-print("**************************************************************************")
-print("                       Program: Git Repo Updater                          ")
-print("                     Author: David Velasco Garcia                         ")
-print("                             @davidvelascogarcia                          ")
-print("**************************************************************************")
-print("**************************************************************************")
 
-print("")
-print("Starting system ...")
-print("")
+class GitRepoUpdater:
 
-print("")
-print("Loading gitRepoUpdater engine ...")
-print("")
+    # Function: Constructor
+    def __init__(self):
+        # Build Halo spinner
+        self.systemResponse = Halo(spinner='dots')
 
-# Get system configuration
-print("")
-print("Detecting system and release version ...")
-print("")
-systemPlatform = platform.system()
-systemRelease = platform.release()
+    # Function: getSystemPlatform
+    def getSystemPlatform(self):
+        # Get system configuration
+        print("\nDetecting system and release version ...\n")
+        systemPlatform = platform.system()
+        systemRelease = platform.release()
 
-print("")
-print("**************************************************************************")
-print("Configuration detected:")
-print("**************************************************************************")
-print("")
-print("Platform:")
-print(systemPlatform)
-print("Release:")
-print(systemRelease)
+        print("**************************************************************************")
+        print("Configuration detected:")
+        print("**************************************************************************")
+        print("\nPlatform:")
+        print(systemPlatform)
+        print("Release:")
+        print(systemRelease)
 
-# Get initial time
-initialTime = datetime.datetime.now()
+        return systemPlatform, systemRelease
 
-print("")
-print("**************************************************************************")
-print("Update Local Repositories")
-print("**************************************************************************")
-print("")
-print("[INFO] Searching local repositories ...")
-print("")
+    # Function: repoUpdater
+    def repoUpdater(self):
 
-# Go to root path
-try:
-    os.chdir("./../../")
+        # Get initial time
+        initialTime = datetime.datetime.now()
 
-except:
-    print("")
-    print("[ERROR] Error moving to root path.")
-    print("")
+        print("**************************************************************************")
+        print("Update Local Repositories")
+        print("**************************************************************************")
 
-# Variable to count repo updated
-repoUpdatedCount = 1
+        systemResponseMessage = "\n[INFO] Searching local repositories ...\n"
+        self.systemResponse.text_color = "blue"
+        self.systemResponse.info(systemResponseMessage)
 
-# Update all local repositories in root path
-for repositorie in os.listdir("."):
+        # Go to root path
+        try:
+            os.chdir("./../../")
 
-    print("")
-    print("[INFO] Local repositorie founded: " + str(repositorie))
-    print("")
+        except:
+            systemResponseMessage = "\n[[ERROR] Error moving to root path.\n"
+            self.systemResponse.text_color = "red"
+            self.systemResponse.fail(systemResponseMessage)
 
-    print("")
+        # Variable to count repo updated
+        repoUpdatedCount = 1
+
+        # Update all local repositories in root path
+        for repo in os.listdir("."):
+
+            systemResponseMessage = "\n[INFO] Local repo founded: " + str(repo) + "\n"
+            self.systemResponse.text_color = "blue"
+            self.systemResponse.info(systemResponseMessage)
+
+            print("**************************************************************************")
+            print("Updating repo " + str(repo))
+            print("**************************************************************************")
+
+            systemResponseMessage = "\n[INFO] Updating repo " + str(repo) + ", " + str(repoUpdatedCount) + "/" + str(len(os.listdir("."))) + " ...\n"
+            self.systemResponse.text_color = "yellow"
+            self.systemResponse.warn(systemResponseMessage)
+
+            # Move to local path
+            try:
+                os.chdir(str(repo))
+
+            except:
+                systemResponseMessage = "\n[ERROR] Error moving to " + str(repo) + " repo.\n"
+                self.systemResponse.text_color = "red"
+                self.systemResponse.fail(systemResponseMessage)
+
+            # Execute update repo
+            try:
+                os.system("git pull")
+
+                systemResponseMessage = "\n[INFO] " + str(repo) + " repo updated correctly.\n"
+                self.systemResponse.text_color = "green"
+                self.systemResponse.succeed(systemResponseMessage)
+
+            except:
+
+                systemResponseMessage = "\n[ERROR] Error updating " + str(repo) + " repo.\n"
+                self.systemResponse.text_color = "red"
+                self.systemResponse.fail(systemResponseMessage)
+
+            # Return to root path
+            try:
+                os.chdir("./../")
+
+            except:
+                systemResponseMessage = "\n[ERROR] Error moving to root path.\n"
+                self.systemResponse.text_color = "red"
+                self.systemResponse.fail(systemResponseMessage)
+
+            # Count repoUpdatedCount + 1
+            repoUpdatedCount = repoUpdatedCount + 1
+
+        print("**************************************************************************")
+        print("Local Repositories Updated")
+        print("**************************************************************************")
+
+        systemResponseMessage = "\n[INFO] Local repositories updated correctly.\n"
+        self.systemResponse.text_color = "green"
+        self.systemResponse.succeed(systemResponseMessage)
+
+        # Get end time and elapsedTime
+        endTime = datetime.datetime.now()
+        elapsedTime = endTime - initialTime
+
+        systemResponseMessage = "\n[INFO] Elapsed time: " + str(elapsedTime) + "\n"
+        self.systemResponse.text_color = "blue"
+        self.systemResponse.info(systemResponseMessage)
+
+
+# Function: main
+def main():
+
     print("**************************************************************************")
-    print("Updating repositorie " + str(repositorie))
     print("**************************************************************************")
-    print("")
-    print("[INFO] Updating repositorie " + str(repositorie) + ", " + str(repoUpdatedCount) + "/" + str(len(os.listdir("."))) + " ...")
-    print("")
+    print("                       Program: Git Repo Updater                          ")
+    print("                     Author: David Velasco Garcia                         ")
+    print("                             @davidvelascogarcia                          ")
+    print("**************************************************************************")
+    print("**************************************************************************")
 
-    # Move to local path
-    try:
-        os.chdir(str(repositorie))
+    print("\nLoading Git Repo Updater engine ...\n")
 
-    except:
-        print("")
-        print("[ERROR] Error moving to " + str(repositorie) + " repositorie.")
-        print("")
+    # Build cryptoFileSecurityEncoder object
+    gitRepoUpdater = GitRepoUpdater()
 
-    # Execute update repositorie
-    try:
-        os.system("git pull")
+    # Get system platform
+    systemPlatform, systemRelease = gitRepoUpdater.getSystemPlatform()
 
-        print("")
-        print("[INFO] " + str(repositorie) + " repositorie updated correctly.")
-        print("")
+    # Update repos
+    gitRepoUpdater.repoUpdater()
 
-    except:
-        print("")
-        print("[ERROR] Error updating " + str(repositorie) + " repositorie.")
-        print("")
+    print("\n**************************************************************************")
+    print("Program finished")
+    print("**************************************************************************")
+    print("\ngitRepoUpdater program finished correctly.\n")
 
-    # Return to root path
-    try:
-        os.chdir("./../")
+    userExit = input()
 
-    except:
-        print("")
-        print("[ERROR] Error moving to root path.")
-        print("")
+if __name__ == "__main__":
 
-    # Count repoUpdatedCount + 1
-    repoUpdatedCount = repoUpdatedCount + 1
+    # Call main function
+    main()
 
-print("")
-print("**************************************************************************")
-print("Local Repositories Updated")
-print("**************************************************************************")
-print("")
-print("[INFO] Local repositories updated correctly.")
-print("")
-
-# Get end time
-endTime = datetime.datetime.now()
-
-# Get elapsedTime
-elapsedTime = endTime - initialTime
-
-print("")
-print("[INFO] Elapsed time: " + str(elapsedTime))
-print("")
-
-print("")
-print("")
-print("**************************************************************************")
-print("Program finished")
-print("**************************************************************************")
-print("")
-print("gitRepoUpdater program finished correctly. ")
-print("")
-endProgram = input()
